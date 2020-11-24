90774a92009f9d3d4f94f492db948ef04d5cdbe7
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-24 10:29:22 -0800

    update json & add images

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
 
-    if(this.props.data){
+   if(this.props.data){
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
+      var business = this.props.data.address.business;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
@@ -14,13 +15,13 @@ class About extends Component {
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
-    }
+   }
 
     return (
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Kambui profile picture" />
+            <img className="profile-pic" src={profilepic} alt="Kambui profile picture" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -31,6 +32,7 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
+                     <span>{business}</span><br />
 						   <span>{street}<br />
 						         {city} {state}, {zip}
                    </span><br />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,20 +4,23 @@ import ParticlesBg  from "particles-bg";
 class Header extends Component {
   render() {
 
-    if(this.props.data){
-       var project = this.props.data.project;
-       var github = this.props.data.github;
+   if(this.props.data){
+      var profilepic= "images/"+this.props.data.image;
+      var project = this.props.data.project; 
+      var github = this.props.data.github;
       var name = this.props.data.name;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+      return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+   })
+   }
 
     return (
       <header id="home">
-      <ParticlesBg color="#ff0000" num={10} type="circle" bg={true} />
+      <img src={profilepic} style={{  'opacity': '0.3'
+      }} />
+      {/* <ParticlesBg color="#ff0000" num={10} type="circle" bg={true} /> */}
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -35,7 +38,7 @@ class Header extends Component {
       
          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
+            <h3>{description}</h3>
             <hr />
             <ul className="social">
                <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
